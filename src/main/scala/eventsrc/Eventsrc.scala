ec106e7dfe22ae4324eb0ecfed71079b2f25915e
Anders Bohn Jespersen <andersbohn@gmail.com>
2021-07-08 06:08:30 +0200

    basic piece running

--- src/main/scala/eventsrc/Eventsrc.scala
+++ src/main/scala/eventsrc/Eventsrc.scala
@@ -4,9 +4,9 @@ import zio.*
 import zio.console.*
 import zio.blocking.*
 import zio.stream.*
-import domain.{Event, EventRaw}
+import domain.{ Event, EventRaw }
 
-import java.io.{BufferedReader, IOException, InputStream, InputStreamReader}
+import java.io.{ BufferedReader, IOException, InputStream, InputStreamReader }
 import java.nio.file.Path
 import java.time.LocalDateTime
 
@@ -16,11 +16,8 @@ case class EventFile(inputStream: InputStream)
 object Eventsrc {
 
   trait Service {
-    def eventStream: ZStream[Blocking, Throwable, Event]
+    def eventStream: ZStream[Blocking, Throwable, Either[Throwable,Event]]
   }
-
-  val liveBlackBoxStream: ZLayer[Has[BlackBoxPath], Nothing, EventsrcService] =
-    ZLayer.fromService[BlackBoxPath, Eventsrc.Service](EventFromBlackBox.spinUpAndStream)
 
   val liveZstream: ZLayer[Has[EventFile], Nothing, EventsrcService] =
     ZLayer.fromService[EventFile, Eventsrc.Service](EventFromFileImpl.eventFileService)
@@ -28,34 +25,25 @@ object Eventsrc {
 
 case object EventFromBlackBox {
 
-  def spinUpBuffer(file: BlackBoxPath): ZIO[Console, Throwable, Unit] = {
+  def spinUpBuffer(file: BlackBoxPath): ZIO[Console with Blocking, Throwable, Unit] = {
     def acquire(file: String) = ZIO.effect {
       val prc = sys.runtime.exec(file)
-      val br  = new BufferedReader(new InputStreamReader(prc.getInputStream, "UTF-8"))
-      prc -> br
+      prc -> EventFile(prc.getInputStream)
     }
 
-    def release(prc: Process, reader: BufferedReader) = ZIO.effectTotal {
+    def release(prc: Process, reader: EventFile) = ZIO.effectTotal {
       prc.destroy()
-      reader.close()
+      reader.inputStream.close()
     }
 
     ZManaged.make(acquire(file.localFile))(release).use { (prc, reader) =>
       (for {
-        str  <- Task.effect(reader.readLine())
-        raw  <- EventRaw.parseJson(str).either
-        event = raw.flatMap(Event.fromRaw)
-        _    <- console.putStrLn(s"> $event") // FIXME aaarg
+        event <- EventFromFileImpl.eventFileService(reader).eventStream.either.runDrain
+        _     <- console.putStrLn(s"> $event") // FIXME aaarg
       } yield ()).forever
     }
   }
 
-  def spinUpAndStream(blackBox: BlackBoxPath) =
-    new Eventsrc.Service {
-      override def eventStream: ZStream[Blocking, IOException, Event] =
-        ???
-
-    }
 
 }
 
@@ -64,19 +52,20 @@ case object EventFromFileImpl {
   def eventFileService(file: EventFile) =
     new Eventsrc.Service {
 
-      def parseEvent(s:String): ZIO[Blocking, Throwable, Event] =
+      def parseEvent(s: String): ZIO[Blocking, Throwable, Event] =
         for {
-          raw <- EventRaw.parseJson(s)
+          raw   <- EventRaw.parseJson(s)
           event <- Task.fromEither(Event.fromRaw(raw))
         } yield event
 
-      override def eventStream: ZStream[Blocking, Throwable, Event] =
-        ZStream.fromInputStream(file.inputStream)
+      override def eventStream: ZStream[Blocking, Throwable, Either[Throwable, Event]] =
+        ZStream
+          .fromInputStream(file.inputStream)
           .chunkN(1)
           .aggregate(ZTransducer.utf8Decode)
           .aggregate(ZTransducer.splitLines)
           .mapChunks(identity)
-          .mapM(parseEvent)
+          .mapM(parseEvent(_).either)
 
     }
 
